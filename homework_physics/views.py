c315d34445a1999d30a4bcc45dcf8025281645e2
jay1224-jay <wuj20061224@gmail.com>
2023-09-15 21:15:56 +0800

    able to distinguish the keyword "斜拋"(projectile motion) from user's message

--- homework_physics/views.py
+++ homework_physics/views.py
@@ -13,6 +13,48 @@ from linebot.models import MessageEvent, TextSendMessage
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
  
+def show_pm(angle, velocity):
+    # 6x6 grid
+
+    #     o
+    #   o  o
+    #  o    o
+    #        o
+    #         o
+
+    s = \
+    " o    " + "\n" + \
+    "o o   " + "\n" + \
+    "   o  " + "\n" + \
+    "    o "
+
+    print(s)
+    return s 
+
+def message_response(text):
+
+    response = ""
+
+    if "斜拋" in text:
+        response =  "Projectile motion!!!\n\n如果想要顯示斜拋模擬圖，請輸入 \'pm angle velocity\'\n比如: pm 30 10\n代表模擬仰角30度，初速度為10的斜拋"
+    elif "pm" in text:
+        # further test
+
+        try:
+            angle = int(text.split()[1])
+        except:
+            return "如果想要顯示斜拋模擬圖，請輸入 \'pm angle velocity\'\n比如: pm 30 10\n代表模擬仰角30度，初速度為10的斜拋"
+        try:
+            velocity = int(text.split()[2])
+        except:
+            return "如果想要顯示斜拋模擬圖，請輸入 \'pm angle velocity\'\n比如: pm 37 5\n代表模擬仰角37度，初速度為5的斜拋"
+
+        response = show_pm(angle, velocity)
+
+    else:
+        response = text[::-1]
+
+    return response
  
 @csrf_exempt
 def callback(request):
@@ -32,7 +74,10 @@ def callback(request):
             if isinstance(event, MessageEvent):  # 如果有訊息事件
                 line_bot_api.reply_message(  # 回復傳入的訊息文字
                     event.reply_token,
-                    TextSendMessage(text=event.message.text + "from Python LineBot.\n Yeah.")
+                    [ 
+                        TextSendMessage(text=message_response(event.message.text))
+                    ]
+                        
                 )
         return HttpResponse()
     else:
